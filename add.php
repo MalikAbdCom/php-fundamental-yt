d595ef3ca5be47792d896495749cf42d9934c25f
malik abdulllah <emailmalikabdcom@gmail.com>
2022-06-10 08:13:47 +0700

    form validation, keep input while value is error

--- add.php
+++ add.php
@@ -1,8 +1,10 @@
 <?php
+$pesan = $alamat = $email = '';
 $arrError = array("email" => '', "pesan" => '', "alamat"=> '');
 if (isset($_POST['submit'])) {
     //                  email
     if ($_POST['email']) {
+        $email = $_POST['email']; //string isi input
         if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
             $arrError['email'] = "your email is invalid woeeee !!!";
         } else {
@@ -16,6 +18,7 @@ if (isset($_POST['submit'])) {
     if (!$_POST['alamat']){
         $arrError['alamat'] = "please provide you alamat bambang !!!";
     }else{
+        $alamat = $_POST['alamat'];
         if (!preg_match("/^[a-z]*$/", $_POST["pesan"])){
             $arrError['alamat'] = "alamat mu engga valid bambang";
         }else{
@@ -24,6 +27,7 @@ if (isset($_POST['submit'])) {
     }
     //                  pesan
     if ($_POST['pesan']) {
+        $pesan = $_POST['pesan'];
         if (!preg_match("/^[a-z]*$/", $_POST["pesan"])) {
             $arrError['pesan'] = "your pesan is invalid gaes !!!";
         } else {
@@ -63,13 +67,13 @@ if (isset($_POST['submit'])) {
     <div class="container form-container">
         <form method="post" action="./add.php" class="white form-div">
             <label for="email">Your Email :</label>
-            <input type="text" name="email" id="email">
+            <input type="text" name="email" id="email" value="<?php echo htmlspecialchars($email)?>" >
             <div class="red-text error-message"><?php echo  $arrError['email'] ?></div>
             <label for="asdf">Your Address :</label>
-            <input type="text" name="alamat" id="asdf">
+            <input type="text" name="alamat" id="asdf" value="<?php echo htmlspecialchars($alamat)?>" >
             <div class="red-text error-message"><?php echo  $arrError['alamat'] ?></div>
             <label for="rrr">Your Message :</label>
-            <input type="text" name="pesan" id="rrr">
+            <input type="text" name="pesan" id="rrr" value="<?php echo htmlspecialchars($pesan)?>">
             <div class="red-text error-message"><?php echo  $arrError['pesan'] ?></div>
             <div class="center">
                 <input type="submit" value="Submit" name="submit" class="btn brand">
